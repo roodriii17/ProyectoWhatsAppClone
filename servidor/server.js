def35b701e858e7c5cfba34e41d9db40c7c200c2
Pr83052@gmail.com <Pr83052@gmail.com>
2025-02-25 22:50:19 +0100

    subida de proyecto

--- servidor/server.js
+++ servidor/server.js
@@ -8,8 +8,11 @@ const io = new Server(server);
 const port = process.env.PORT || 3000;
 
 let usuariosConectados = [];
+let salas = ['General'];
+let mensajesPorSala = {
+  'General': [],
+};
 
-// Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -22,8 +25,8 @@ io.on('connection', (socket) => {
     usuariosConectados.push(usuario);
     io.emit('actualizarUsuarios', usuariosConectados);
     io.emit('mensaje', { 
-      nombre: usuario.nombre, 
-      imagen: usuario.imagen, 
+      nombre: 'Servidor', 
+      imagen: 'public/img/server_avatar.png', 
       mensaje: `${usuario.nombre} se ha unido al chat` 
     });
   });
@@ -31,47 +34,37 @@ io.on('connection', (socket) => {
   socket.on('unirseSala', (sala) => {
     socket.join(sala);
     socket.salaActual = sala;
-    socket.emit('mensaje', { nombre: 'Servidor', mensaje: `Te has unido a la sala ${sala}` });
+    socket.emit('mensajesSala', mensajesPorSala[sala]);
+    socket.emit('mensaje', { 
+      nombre: 'Servidor', 
+      imagen: 'public/img/server_avatar.png', 
+      mensaje: `Te has unido a la sala ${sala}` 
+    });
   });
 
   socket.on('mensaje', (datos) => {
+    mensajesPorSala[socket.salaActual].push(datos);
     io.to(socket.salaActual).emit('mensaje', datos);
   });
 
-  socket.on('escribiendo', (usuario) => {
-    socket.broadcast.to(socket.salaActual).emit('escribiendo', usuario);
+  socket.on('archivo', (datos) => {
+    mensajesPorSala[socket.salaActual].push(datos);
+    io.to(socket.salaActual).emit('archivo', datos);
   });
 
-  socket.on('mensajePrivado', (datos) => {
-    const destinatario = usuariosConectados.find(u => u.nombre === datos.destinatario);
-    if (destinatario) {
-      const socketDestinatario = Array.from(io.sockets.sockets.values()).find(s => s.usuario.nombre === datos.destinatario);
-      if (socketDestinatario) {
-        const salaPrivada = `privado-${socket.usuario.nombre}-${datos.destinatario}`;
-        socket.join(salaPrivada);
-        socketDestinatario.join(salaPrivada);
-        io.to(salaPrivada).emit('mensajePrivado', { 
-          nombre: socket.usuario.nombre, 
-          imagen: socket.usuario.imagen, 
-          mensaje: datos.mensaje 
-        });
-      }
-    }
+  socket.on('escribiendo', (nombre) => {
+    socket.broadcast.emit('escribiendo', nombre);
   });
 
   socket.on('escribiendoPrivado', (datos) => {
-    const destinatario = usuariosConectados.find(u => u.nombre === datos.destinatario);
-    if (destinatario) {
-      const socketDestinatario = Array.from(io.sockets.sockets.values()).find(s => s.usuario.nombre === datos.destinatario);
-      if (socketDestinatario) {
-        const salaPrivada = `privado-${socket.usuario.nombre}-${datos.destinatario}`;
-        socketDestinatario.emit('escribiendoPrivado', { nombre: socket.usuario.nombre, sala: salaPrivada });
-      }
-    }
+    socket.broadcast.emit('escribiendoPrivado', datos);
   });
 
-  socket.on('archivo', (datos) => {
-    io.to(socket.salaActual).emit('archivo', datos);
+  socket.on('mensajePrivado', (datos) => {
+    const destinatarioSocket = Array.from(io.sockets.sockets.values()).find(s => s.usuario && s.usuario.nombre === datos.destinatario);
+    if (destinatarioSocket) {
+      destinatarioSocket.emit('mensajePrivado', datos);
+    }
   });
 
   socket.on('disconnect', () => {
@@ -79,7 +72,8 @@ io.on('connection', (socket) => {
       usuariosConectados = usuariosConectados.filter(u => u.nombre !== socket.usuario.nombre);
       io.emit('actualizarUsuarios', usuariosConectados);
       io.emit('mensaje', { 
-        nombre: socket.usuario.nombre, 
+        nombre: 'Servidor', 
+        imagen: 'public/img/server_avatar.png', 
         mensaje: `${socket.usuario.nombre} ha salido del chat` 
       });
     }
